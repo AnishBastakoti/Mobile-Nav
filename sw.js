8591430f95161e422d0d0d96540c33f702e4dff1
AnishBastakoti <anishbastakoti7@gmail.com>
2021-06-30 23:53:27 +0930

    files added

---
+++ sw.js
@@ -0,0 +1,38 @@
+var cacheName = 'Cache';
+var urlsToCache = [
+  '/',
+  'index.html',
+  '/style.css',
+];
+self.addEventListener("install", (e) => {
+  console.log("Service Worker: Installed");
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then((cache) => {
+        console.log("Service Worker: Caching Files");
+        cache.addAll(urlsToCache);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+self.addEventListener("activate", (e) => {
+  console.log("Service Worker: Activated");
+  // Remove unwanted caches
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== cacheName) {
+            console.log("Service Worker: Clearing Old Cache");
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+self.addEventListener("fetch", (e) => {
+  console.log("Service Worker: Fetching");
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
